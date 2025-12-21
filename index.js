0935f6a99a2da7f9fe765d0921fb097928b48720
Zoey Chesnut <zoeychesnut@MacBookAir.cinci.rr.com>
2025-12-21 15:15:07 -0500

    fixed

--- index.js
+++ index.js
@@ -4,6 +4,21 @@
 
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1/20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    
+    for(let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
@@ -47,3 +62,4 @@ function toggleModal() {
     isModalOpen = true;
     document.body.classList += " modal--open"
 }
+
